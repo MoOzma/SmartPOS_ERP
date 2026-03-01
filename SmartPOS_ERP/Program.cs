178b04706f6c1fdeff7592429905847d1e2349ef
MoZoom <160332912+MoZoom@users.noreply.github.com>
2026-03-01 02:41:42 +0200

    h

--- SmartPOS_ERP/Program.cs
+++ SmartPOS_ERP/Program.cs
@@ -5,7 +5,6 @@ using SmartPOS_ERP.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//t
 // 1. إعداد قاعدة البيانات
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
