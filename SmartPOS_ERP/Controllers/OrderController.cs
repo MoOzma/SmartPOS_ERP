fa4cb5ed399ad84dde8ae686895deae2c2cf69d0
MoZoom <160332912+MoZoom@users.noreply.github.com>
2026-03-08 21:33:48 +0200

    03023032

--- SmartPOS_ERP/Controllers/OrderController.cs
+++ SmartPOS_ERP/Controllers/OrderController.cs
@@ -16,6 +16,7 @@ namespace SmartPOS_ERP.Controllers
         public OrderController(ApplicationDbContext context)
         {
             _context = context;
+            ///;;;;;
         }
         public async Task<IActionResult> Index()
         {
